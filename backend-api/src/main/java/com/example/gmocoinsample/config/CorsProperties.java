d1cf5310c040c592f14743a962558d74a70c0682
Masataka-Nakamura-ggs <m-nakamura.ggs@gmofh.com>
2025-08-15 15:24:00 +0900

    feat: CORS設定用のプロパティクラスを追加し、DockerfileのNode.jsバージョンを更新
    - CorsProperties.javaを新規作成し、CORS設定のプロパティを管理
    - Dockerfileで使用するNode.jsのバージョンを20から22.18.0に更新
    - Dockerfile内でパッケージを最新化するコマンドを追加

---
+++ backend-api/src/main/java/com/example/gmocoinsample/config/CorsProperties.java
@@ -0,0 +1,21 @@
+package com.example.gmocoinsample.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "cors") // ymlの "cors" プレフィックスを指定
+public class CorsProperties {
+
+    @Getter @Setter
+    private String allowedOrigins;
+    @Getter @Setter
+    private String allowedMethods;
+    @Getter @Setter
+    private String allowedHeaders;
+    @Getter @Setter
+    private boolean allowCredentials;
+}
